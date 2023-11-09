3421e7b89b313c7213db61a75ba763a7374db820
EL HILA ZIYAD <ziyad.el-hila@epitech.eu>
2023-11-09 10:34:10 +0100

    create menu setup

---
+++ core/src/fr/epitale/game/MenuScreen.java
@@ -0,0 +1,59 @@
+package fr.epitale.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+public class MenuScreen implements Screen {
+    Main game;
+    Texture logo;
+    Texture playButton;
+    Texture exitButton;
+
+    public MenuScreen(Main game) {
+        this.game = game;
+        logo = new Texture("Menu/Game_Logo.png");
+        playButton = new Texture("Menu/play_Button.png");
+        exitButton = new Texture("Menu/exit_Button.png");
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(logo, 100, 100);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+    }
+
+}
