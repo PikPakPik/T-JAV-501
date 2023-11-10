5b28e74c9c2981060c748caff77f3106f3076f42
Alexandre.T <alexandre.tressel@icloud.com>
2023-11-10 21:58:36 +0100

    refactor: :recycle: Delete unused imports

--- core/src/fr/epitale/game/Main.java
+++ core/src/fr/epitale/game/Main.java
@@ -3,8 +3,6 @@ package fr.epitale.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import fr.epitale.game.Map.Epitale;
-
 public class Main extends Game {
 	public static final int WIDTH = 1280;
 	public static final int HEIGHT = 720;
