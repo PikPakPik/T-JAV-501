9382950a0e7854d639eccaaf6216887ea07a99d3
Alexandre.T <alexandre.tressel@icloud.com>
2023-11-13 14:10:04 +0100

    docs: change name of windows and add default size

--- desktop/src/fr/epitale/game/DesktopLauncher.java
+++ desktop/src/fr/epitale/game/DesktopLauncher.java
@@ -3,14 +3,13 @@ package fr.epitale.game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main(String[] arg) {
+	public static void main (String[] arg) {
+
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1280*2, 720*2);
 		config.setForegroundFPS(60);
-		config.setTitle("Epitale");
-		config.setWindowedMode(Main.WIDTH, Main.HEIGHT);
-		config.setResizable(false);
+		config.setTitle("Projet JAV-501 | Epitale");
 		new Lwjgl3Application(new Main(), config);
 	}
 }
