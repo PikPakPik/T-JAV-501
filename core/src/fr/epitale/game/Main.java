fcfce1731fb3a06de86b9e8d9775432a76ab8e46
EL HILA ZIYAD <ziyad.el-hila@epitech.eu>
2023-11-13 12:12:13 +0100

    create map

--- core/src/fr/epitale/game/Main.java
+++ core/src/fr/epitale/game/Main.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import fr.epitale.game.Map.Epitale;
+import fr.epitale.game.MiniGame.EpiDash.EpiDash;
 
 public class Main extends Game {
 	public static final int WIDTH = 1280;
@@ -15,10 +16,11 @@ public class Main extends Game {
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		background = new Background();
-		background.create();
-		this.setScreen(new MenuScreen(this, background));
+		// background = new Background();
+		// background.create();
+		// this.setScreen(new MenuScreen(this, background));
 		// setScreen(new Epitale(this));
+		this.setScreen(new EpiDash(this));
 
 	}
 }
---
+++ core/src/fr/epitale/game/MiniGame/EpiDash/EpiDash.java
@@ -0,0 +1,63 @@
+package fr.epitale.game.MiniGame.EpiDash;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+import fr.epitale.game.Map.Epitale;
+
+public class EpiDash implements Screen {
+    final Epitale game;
+    private OrthographicCamera camera;
+
+    public EpiDash(final Epitale game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    @Override
+    public void show() {
+        game.tiledMap = new TmxMapLoader().load("EpitechDash.tmx");
+        game.tiledMapRenderer = new OrthogonalTiledMapRenderer(game.tiledMap);
+        game.character.setX(39*16);
+        game.character.setY(0);
+    }
+
+    @Override
+    public void render(float delta) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resize'");
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pause'");
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resume'");
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hide'");
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dispose'");
+    }
+}
