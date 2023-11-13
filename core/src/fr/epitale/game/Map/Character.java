d1340e60d2718aa8e61c4b3c02977f84fce9b389
Alexandre.T <alexandre.tressel@icloud.com>
2023-11-13 14:08:16 +0100

    refacto: refactor character class

--- core/src/fr/epitale/game/Map/Character.java
+++ core/src/fr/epitale/game/Map/Character.java
@@ -1,50 +1,40 @@
 package fr.epitale.game.Map;
+
 public class Character {
     private float x;
     private float y;
-    private boolean isVisible = true;
-    private float speed;
+    private final boolean isVisible;
+    private final float speed;
 
     public Character(float initialX, float initialY) {
-        x = initialX;
-        y = initialY;
-        speed = 1.5f;
+        this.x = initialX;
+        this.y = initialY;
+        this.isVisible = true;
+        this.speed = 1.5f;
     }
 
     public void move(float deltaX, float deltaY) {
-        x += deltaX * speed;
-        y += deltaY * speed;
+        this.x += deltaX * this.speed;
+        this.y += deltaY * this.speed;
     }
 
     public float getX() {
-        return x;
+        return this.x;
     }
 
     public float getY() {
-        return y;
+        return this.y;
     }
+
     public float getSpeed() {
-        return speed;
+        return this.speed;
     }
 
     public void setX(float newX) {
-        x = newX;
+        this.x = newX;
     }
 
     public void setY(float newY) {
-        y = newY;
+        this.y = newY;
     }
-
-    public void setSpeed(float newSpeed) {
-        speed = newSpeed;
-    }
-
-    public boolean isVisible() {
-        return isVisible;
-    }
-
-    public void setVisible(boolean newVisible) {
-        isVisible = newVisible;
-    }
-
 }
